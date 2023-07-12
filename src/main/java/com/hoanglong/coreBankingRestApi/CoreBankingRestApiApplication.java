12f3b972d7cb47bd1aef0daf465d85be23b9ea9d
hoanglong5 <longch.dev@gmail.com>
2023-07-12 13:13:48 +0700

    Init project

---
+++ src/main/java/com/hoanglong/coreBankingRestApi/CoreBankingRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.hoanglong.coreBankingRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreBankingRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreBankingRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoanglong/coreBankingRestApi/CoreBankingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoanglong.coreBankingRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreBankingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
